789a66558df7916e8660f4e0a0fa44c358eb2732
CesarQuinonezEs <quinonez.cesar@uabc.edu.mx>
2023-08-14 15:36:22 -0700

    prueba

--- src/main/java/selenium/seleniumMvn/App.java
+++ src/main/java/selenium/seleniumMvn/App.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
+
 public class App 
 {
     public static void main( String[] args )
